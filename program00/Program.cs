a79fcfd3564ab91ac7c33299ad0025bff314dc0e
Student <foobar@sr.htlweiz.at>
2025-11-27 15:45:59 +0100

    first things done

---
+++ program00/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using binary_converter;
+
+namespace Program00{
+    public class Program00{
+        public static int Main(string[] argv){
+            converter Converter = new converter("011031");
+            if(Converter.binary_check()){
+                Console.WriteLine("is Binary");
+            }else{
+                Console.WriteLine("Is not Binary");
+            }
+            return 0;
+        }
+    }
+}
